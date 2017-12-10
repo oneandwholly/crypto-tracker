d4d10e35d8f48a7daaeb33a884509d4702d73638
Jon Huh <oneandwholly@gmail.com>
2017-12-09 19:27:16 -0800

    add constants

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Store from './src/Store';
-import { Header, CryptoContainer } from './src/components';
+import { Header, CryptoContainer } from './src/Components';
 
 
 
